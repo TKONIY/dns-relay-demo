f47ddfdc50de73ed6cb3f330be181a1cbfe6eb8e
TKONIY <yangshen.d@bupt.edu.cn>
2020-09-11 02:01:31 +0800

    remove sqlite3

--- source_code/database.c
+++ source_code/database.c
@@ -3,7 +3,7 @@
 #endif
 
 #include "database.h"
-#include "sqlite3.h" /*仅该模块操作sqlite*/
+//#include "sqlite3.h" /*仅该模块操作sqlite*/
 
 static CQueue clientTable;
 
@@ -118,87 +118,87 @@ int GetCTableFrontIndex() {
 //static sqlite3* db = NULL;  /*数据库对象*/
 
 /*封装INSERT接口*/
-static int DNSInsert(sqlite3* db, const char* ip, const char* name) {
-	/*char sql_insert[512] = "INSERT INTO DNS_record (IP, Name) VALUES('";
-	strcat(sql_insert, ip);
-	strcat(sql_insert, "', '");
-	strcat(sql_insert, name);
-	strcat(sql_insert, "');");*/
-
-	char sql_insert[512];
-	sprintf(sql_insert,
-		"INSERT INTO DNS_record (IP, Name) "
-		"VALUES('%s', '%s');",
-		ip, name);
-	/*"INSERT INTO DNS_record (IP, Name)"
-	"VALUES('49.234.121.191', 'canteencloud.com');";*/
-	return sqlite3_exec(db, sql_insert, NULL, NULL, NULL);
-}
+//static int DNSInsert(sqlite3* db, const char* ip, const char* name) {
+//	/*char sql_insert[512] = "INSERT INTO DNS_record (IP, Name) VALUES('";
+//	strcat(sql_insert, ip);
+//	strcat(sql_insert, "', '");
+//	strcat(sql_insert, name);
+//	strcat(sql_insert, "');");*/
+//
+//	char sql_insert[512];
+//	sprintf(sql_insert,
+//		"INSERT INTO DNS_record (IP, Name) "
+//		"VALUES('%s', '%s');",
+//		ip, name);
+//	/*"INSERT INTO DNS_record (IP, Name)"
+//	"VALUES('49.234.121.191', 'canteencloud.com');";*/
+//	return sqlite3_exec(db, sql_insert, NULL, NULL, NULL);
+//}
 
 /*封装SELECT接口*/
-static int DNSSelect(sqlite3* db, const char* name, char* ip) {
-	/*char sql_select[512] = "SELECT IP FROM DNS_record WHERE Name = '";
-	strcat(sql_select, name);
-	strcat(sql_select, "';");*/
-
-	char sql_select[512];
-	sprintf(sql_select,
-		"SELECT IP "
-		"FROM DNS_record "
-		"WHERE Name = '%s';",
-		name);
-	 debugPrintf("%s\n", sql_select);
-	/*"SELECT IP"
-	"FROM DNS_record"
-	"WHERE Name = 'canteencloud.com';";*/
-	char** result = NULL;/* 查询结果的指针*/
-	int nRow;	/*行数*/
-	int nCol;	/*列数*/
-	int rc = sqlite3_get_table(db, sql_select, &result, &nRow, &nCol, NULL);
-
-	if (SQLITE_OK == rc && nRow > 0) {
-		/*
-			result表格的构造:
-			将 name, ip,
-			   n1,	0.0.0.0,
-			   n2,  0.0.0.0, ...
-			变成一维数组
-			name, ip, n1, 0.0.0.0, n2, 0.0.0.0, n3, 0.0.0.0
-
-			我们只搜IP,那么搜出来的表格就是
-			'IP', ip1, ip2, ip3;
-		*/
-		strcpy(ip, result[1]);
-		sqlite3_free_table(result);
-		return SQLITE_OK;
-	} else {
-		sqlite3_free_table(result);
-		return SQLITE_FAIL;
-	}
-}
+//static int DNSSelect(sqlite3* db, const char* name, char* ip) {
+//	/*char sql_select[512] = "SELECT IP FROM DNS_record WHERE Name = '";
+//	strcat(sql_select, name);
+//	strcat(sql_select, "';");*/
+//
+//	char sql_select[512];
+//	sprintf(sql_select,
+//		"SELECT IP "
+//		"FROM DNS_record "
+//		"WHERE Name = '%s';",
+//		name);
+//	 debugPrintf("%s\n", sql_select);
+//	/*"SELECT IP"
+//	"FROM DNS_record"
+//	"WHERE Name = 'canteencloud.com';";*/
+//	char** result = NULL;/* 查询结果的指针*/
+//	int nRow;	/*行数*/
+//	int nCol;	/*列数*/
+//	int rc = sqlite3_get_table(db, sql_select, &result, &nRow, &nCol, NULL);
+//
+//	if (SQLITE_OK == rc && nRow > 0) {
+//		/*
+//			result表格的构造:
+//			将 name, ip,
+//			   n1,	0.0.0.0,
+//			   n2,  0.0.0.0, ...
+//			变成一维数组
+//			name, ip, n1, 0.0.0.0, n2, 0.0.0.0, n3, 0.0.0.0
+//
+//			我们只搜IP,那么搜出来的表格就是
+//			'IP', ip1, ip2, ip3;
+//		*/
+//		strcpy(ip, result[1]);
+//		sqlite3_free_table(result);
+//		return SQLITE_OK;
+//	} else {
+//		sqlite3_free_table(result);
+//		return SQLITE_FAIL;
+//	}
+//}
 
 /*封装从文件中读取接口*/
-static int DNSImport(sqlite3* db, const char* fname) {
-	FILE* fp = fopen(fname, "r");
-	if (!fp) { //空
-		 debugPrintf("Can't open dnsrealay.txt %s\n", fname);
-		return SQLITE_FAIL;
-	} else {
-		char ip[MAX_IP_BUFSIZE] = { '\0' };
-		char name[MAX_DOMAINNAME] = { '\0' };
-		while (!feof(fp))
-		{
-			fscanf(fp, "%s", ip);
-			fscanf(fp, "%s", name);
-
-			/*insert to db, 不判断结果了*/
-			DNSInsert(db, ip, name);
-			//printf("imported %s %s\n", ip, name);
-		}
-		fclose(fp);
-		return SQLITE_OK;
-	}
-}
+//static int DNSImport(sqlite3* db, const char* fname) {
+//	FILE* fp = fopen(fname, "r");
+//	if (!fp) { //空
+//		 debugPrintf("Can't open dnsrealay.txt %s\n", fname);
+//		return SQLITE_FAIL;
+//	} else {
+//		char ip[MAX_IP_BUFSIZE] = { '\0' };
+//		char name[MAX_DOMAINNAME] = { '\0' };
+//		while (!feof(fp))
+//		{
+//			fscanf(fp, "%s", ip);
+//			fscanf(fp, "%s", name);
+//
+//			/*insert to db, 不判断结果了*/
+//			DNSInsert(db, ip, name);
+//			//printf("imported %s %s\n", ip, name);
+//		}
+//		fclose(fp);
+//		return SQLITE_OK;
+//	}
+//}
 
 /*******************dnsrelay.txt文本数据库封装****************/
 static FILE* dbTXT = NULL;		/*文本数据库对象*/
